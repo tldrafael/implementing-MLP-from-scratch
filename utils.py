0d4b5a35f1ddbdf751478a1c3356a7106d45cd90
Rafael Toledo <rafael.toledo@khomp.com>
2017-09-11 11:13:50 -0300

    remove functions that no more belongs to utils

--- utils.py
+++ utils.py
@@ -8,21 +8,3 @@ def fun_sigmoid(z):
 def fun_sigmoid_derivative(a):
     derv = np.multiply(a, (1.0 - a))
     return derv
-
-
-def compute_mean_squared_error(a_3, idx):
-    y_3 = y_norm[idx].reshape(a_3.shape)
-    err = 0.5*np.power(y_3 - a_3, 2)
-
-    err = np.sum(err)/batch_size
-    return err
-
-
-def compute_log_likelihood(a_3, idx):
-    y_3 = y_norm[idx].reshape(a_3.shape)
-
-    ll = np.multiply(y_3, np.log(a_3)) + np.multiply(1 - y_3, np.log(1 - a_3))
-    ll = np.sum(ll)/batch_size
-
-    return ll
-
