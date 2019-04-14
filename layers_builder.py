8313838532a7598a4e572474b9b0725d621792ec
Rafael Toledo <rafaeltol@gmail.com>
2019-04-14 16:08:29 -0300

    Update code

--- layers_builder.py
+++ layers_builder.py
@@ -1,120 +1,104 @@
+import sys
 import numpy as np
 import utils
 
-# Layer class is a map function which vinculates the *m* units from the layer (l) and link each one to the *n* units from the layer (l+1)
-# the Weight matrix is composed as: rows -> units from Layer(l+1) ; columns -> units from Layer (l)
+
+# Layer class is a map function which vinculates the *m* units from the layer (l) and link each one to
+# the *n* units from the layer (l+1)
+# the Weight matrix is composed by:
+#   + rows -> units from Layer(l+1)
+#   + columns -> units from Layer (l)
+#
 class Layer:
-    
     batch_size = None
 
     def __init__(self, n_units_current, n_units_next, bias, layer_id):
-       
         self.layer_id = layer_id
         self.n_units_current = n_units_current
         self.n_units_next = n_units_next
         self.bias = bias
-        
-        # summation vector
+
+        # Summation vector
         self.z = self.initialize_vector((self.n_units_current, Layer.batch_size))
-                
-        # activation vector
-        ## just inialize the vector, afterwards process as actual activation
+
+        # Activation vector
+        # Inialize the vector and then set the activation function
         self.a = self.initialize_vector((self.n_units_current, Layer.batch_size))
-        self.set_activation()        
-        
-        # weight matrix that connect units from current layer to next layer
-        self.W = self.initialize_weights()        
-        
-        # delta-error vector
+        self.set_activation()
+
+        # Weight matrix that connect units from current layer to next layer
+        self.W = self.initialize_weights()
+
+        # Delta-error vector
         self.d = self.initialize_vector((self.bias + self.n_units_current, Layer.batch_size))
-        
-        # gradient error vector
+
+        # Gradient error vector
         self.g = self.initialize_vector(self.W.shape)
 
-        # gradient approximation vector
+        # Gradient approximation vector
         self.ga = self.initialize_vector(self.g.shape)
 
-
-    
     def initialize_weights(self):
-        # case there's none next layer as the output layer, also there's no Weight matrix
-        if( self.n_units_next == None):
+        # case there's none next layer is the output layer, also there's no Weight matrix
+        if self.n_units_next is None:
             return np.array([])
-        
-        weights = np.random.randn(self.n_units_next * (self.bias + self.n_units_current ))
-        weights = weights.reshape(self.n_units_next, self.bias + self.n_units_current)
-        return weights
+        else:
+            weights = np.random.randn(self.n_units_next * (self.bias + self.n_units_current))
+            weights = weights.reshape(self.n_units_next, self.bias + self.n_units_current)
+            return weights
 
-        
-    
     def initialize_vector(self, n_dimensions):
         return np.random.normal(size=n_dimensions)
 
-    
-
     def set_activation(self):
         self.a = utils.fun_sigmoid(self.z)
-        if self.bias: 
+        if self.bias:
             self.add_activation_bias()
 
-
-    
     def add_activation_bias(self):
         if len(self.a.shape) == 1:
             self.a = np.vstack((1, self.a))
         else:
             self.a = np.vstack((np.ones(self.a.shape[1]), self.a))
 
-
-
     def get_derivative_of_activation(self):
         return utils.fun_sigmoid_derivative(self.a)
 
-
-
     def update_weights(self, r):
-        self.W += -(r*self.g)
-
-
+        self.W += -(r * self.g)
 
     def check_gradient_computation(self, atol):
         return np.allclose(self.g, self.ga, atol=atol)
 
-
-        
     def print_layer(self):
-        print "W:\n %s \n" %(self.W)
-        print "z: %s" %(self.z)
-        print "a: %s" %(self.a)
-        print "d: %s" %(self.d)
-        print "g: %s" %(self.g)
-        
+        print("W:\n {} \n".format(self.W))
+        print("z: {}".format(self.z))
+        print("a: {}".format(self.a))
+        print("d: {}".format(self.d))
+        print("g: {}".format(self.g))
 
 
-        
-# the layer output is an exception case of the Layer class   
-## No summation vector
+# The output layer is an exception case of the Layer class
+# No summation vector
 class LayerInput(Layer):
-        def __init__(self, n_units_current, n_units_next=None, bias=True, layer_id=0):
-            Layer.__init__(self, n_units_current, n_units_next, bias, layer_id)
-            self.z = []
+    def __init__(self, n_units_current, n_units_next=None, bias=True, layer_id=0):
+        Layer.__init__(self, n_units_current, n_units_next, bias, layer_id)
+        self.z = []
 
 
-# the hidden layer must have a link between the current units to next units
+# The hidden layer must have a link between the current units to next units
 class LayerHidden(Layer):
-        def __init__(self, n_units_current, n_units_next, bias=True, layer_id=None):
-            Layer.__init__(self, n_units_current, n_units_next, bias, layer_id)
+    def __init__(self, n_units_current, n_units_next, bias=True, layer_id=None):
+        Layer.__init__(self, n_units_current, n_units_next, bias, layer_id)
 
 
-
-# the layer output is an exception case of the Layer class   
-## No bias, and no Weight matrix
+# The layer output is an exception case of the Layer class
+# No bias, and no Weight matrix
 class LayerOutput(Layer):
-        def __init__(self, n_units_current, layer_id):
-            Layer.__init__(self, n_units_current, n_units_next=None, bias=False, layer_id=None)
-            self.g = []
-            self.ga = []
-
+    def __init__(self, n_units_current, layer_id):
+        Layer.__init__(self, n_units_current, n_units_next=None, bias=False, layer_id=None)
+        self.g = []
+        self.ga = []
 
 
 class ObjLinearRegression(LayerOutput):
@@ -129,46 +113,32 @@ class ObjLinearRegression(LayerOutput):
         return np.ones(shape=self.a.shape)
 
 
-
 class ObjLogisticRegression(LayerOutput):
     def __init__(self, n_units_current, layer_id):
         LayerOutput.__init__(self, n_units_current, layer_id)
         self.objective = 'logistic-reg'
 
 
-    
 def net_constructer(layers_dim, batch_size, objective):
-    if (len(layers_dim) < 2):
+    if len(layers_dim) < 2:
         sys.exit("Neural Net must have at least 2 layers")
-    
-    Layer.batch_size = batch_size    
 
+    Layer.batch_size = batch_size
     net = []
-    # first stage: create input and hidden layers
+    # First stage: create input and hidden layers
     for i in np.arange(len(layers_dim) - 1, dtype=int):
-        if (i == 0):
-            l = LayerInput(layers_dim[i], layers_dim[i+1], bias=True)
-            net.append(l)
-            continue
+        if i == 0:
+            new_layer = LayerInput(layers_dim[i], layers_dim[i + 1], bias=True)
+            net.append(new_layer)
+        else:
+            new_layer = LayerHidden(layers_dim[i], layers_dim[i + 1], bias=True, layer_id=i)
+            net.append(new_layer)
 
-        l = LayerHidden(layers_dim[i], layers_dim[i+1], bias=True, layer_id=i)
-        net.append(l)
-
-
-    # second stage: create output layer
-    l = []
+    # Second stage: create output layer
     if objective == 'linear-reg':
-        l = ObjLinearRegression(layers_dim[-1], layer_id=len(layers_dim))
+        new_layer = ObjLinearRegression(layers_dim[-1], layer_id=len(layers_dim))
     elif objective == 'logistic-reg':
-        l = ObjLogisticRegression(layers_dim[-1], layer_id=len(layers_dim))
+        new_layer = ObjLogisticRegression(layers_dim[-1], layer_id=len(layers_dim))
+    net.append(new_layer)
 
-
-    net.append(l)
     return net
-
-
-    
-
-
-
-
--- neural_net.py
+++ neural_net.py
@@ -2,35 +2,33 @@ import layers_builder
 import numpy as np
 import sys
 
-objective_options = {'linear-reg':'self.mean_squared_error', 'logistic-reg':'self.log_likelihood'}
+objective_options = {'linear-reg': 'self.mean_squared_error',
+                     'logistic-reg': 'self.log_likelihood'}
+
+
 def validate_objective_functions(obj):
     if obj not in objective_options.keys():
-        sys.exit("the objective tasks available are %s" %(objective_options))
-
+        available_objective_functions = ', '.join(list(objective_options.keys()))
+        sys.exit("the objective tasks available are {}." .format(available_objective_functions))
     return obj
 
 
-
 class NeuralNet:
-        
     def __init__(self,  layers_dim, batch_size, objective):
-        self.objective = validate_objective_functions(objective)        
+        self.objective = validate_objective_functions(objective)
         self.objective_fun = eval(objective_options[self.objective])
 
         self.layers_dim = layers_dim
         self.layer_out_id = len(layers_dim) - 1
         self.batch_size = batch_size
         self.net = layers_builder.net_constructer(self.layers_dim, self.batch_size, self.objective)
-        
+
         self.data_X = None
         self.data_Y = None
         self.idx = None
         self.data_X_batch = None
-        
         self.obj_history = []
-        
-        
-    
+
     def compute_gradient_approximation(self, i, weight_shift=1e-4):
         W_shape = self.net[i].W.shape
         for j_w in np.arange(W_shape[1]):
@@ -48,193 +46,148 @@ class NeuralNet:
                 self.net[i].W[i_w, j_w] = self.net[i].W[i_w, j_w] - weight_shift
 
                 # approximate gradient
-                self.net[i].ga[i_w, j_w] = (shift_positive - shift_negative)/(2*weight_shift)                    
-
-
+                self.net[i].ga[i_w, j_w] = (shift_positive - shift_negative)/(2*weight_shift)
 
     def gradient_checking(self):
-        # now do the same manually
         for i in np.arange(0, self.layer_out_id, dtype=int)[::-1]:
             self.compute_gradient_approximation(i)
-            check = self.net[i].check_gradient_computation(atol=1e-1)
-            
-            if not check:
-                print "g:"
-                print self.net[i].g
-                print "\nga:"
-                print self.net[i].ga
+            if not self.net[i].check_gradient_computation(atol=1e-1):
+                print("g:")
+                print(self.net[i].g)
+                print("\nga:")
+                print(self.net[i].ga)
                 sys.exit("Error in compute gradient from layer " + str(self.net[i].layer_id))
-                
-        print "Gradient Checking is Matching!"
-    
-    
-    
+        print("Gradient Checking is Matching!")
+
     def back_propagate_error(self):
-        # Two-stage process: 
-        ## 1st, a distinguish to get the output layer error
-        ## 2nd, a standard computation to get the hidden layers errors
-        
-        # output layer
+        # Two-stage process:
+        #   1. Computation to get the output layer error
+        #   2. Computation to get the hidden layers errors
+
+        # Output layer
         derv_cost_by_activation = -(self.data_Y[self.idx] - self.net[self.layer_out_id].a)
         derv_activation_by_summation = self.net[self.layer_out_id].get_derivative_of_activation()
         self.net[self.layer_out_id].d = np.multiply(derv_cost_by_activation, derv_activation_by_summation)
 
-
-        # hidden layers
+        # Hidden layers
         for i in np.arange(1, self.layer_out_id, dtype=int)[::-1]:
-            d_next = self.net[i+1].d
-            if self.net[i+1].bias: 
+            d_next = self.net[i + 1].d
+            if self.net[i + 1].bias:
                 d_next = d_next[1:]
-            
+
             derv_summation_lnext_by_activation = self.net[i].W.transpose().dot(d_next)
             derv_activation_by_summation = self.net[i].get_derivative_of_activation()
             self.net[i].d = np.multiply(derv_summation_lnext_by_activation, derv_activation_by_summation)
-            
-            
-    
+
     def compute_gradients_errors(self):
-        # update layer errors
+        # Update layer errors
         self.back_propagate_error()
-        
-        # hidden layers
+
+        # Hidden layers
         for i in np.arange(0, self.layer_out_id, dtype=int)[::-1]:
             layer_cur_activations = self.net[i].a
-            layer_next_errors = self.net[i+1].d
-            
-            # if layer next (l+1) has bias, remove its error row
-            if self.net[i+1].bias: 
+            layer_next_errors = self.net[i + 1].d
+            # If layer_next (l+1) has bias, remove its error row
+            if self.net[i + 1].bias:
                 layer_next_errors = layer_next_errors[1:]
-            
-            self.net[i].g = layer_next_errors.dot(layer_cur_activations.transpose()) 
-            # normalize by batch size
-            self.net[i].g = self.net[i].g / self.batch_size
-            
-    
-    
-    def update_weights(self, r, check_grad):
-        # get gradient error for each weight
-        self.compute_gradients_errors()
 
-        if check_grad :
+            self.net[i].g = layer_next_errors.dot(layer_cur_activations.transpose())
+            # Normalize the gradient by batch size
+            self.net[i].g = self.net[i].g / self.batch_size
+
+    def update_weights(self, r, check_grad):
+        # Get gradient error for each weight
+        self.compute_gradients_errors()
+        if check_grad:
             self.gradient_checking()
 
         for i in np.arange(0, self.layer_out_id, dtype=int)[::-1]:
             self.net[i].update_weights(r)
-       
-    
-    
+
     def feed_forward_NN(self):
         for i in np.arange(0, self.layer_out_id + 1, dtype=int):
-            # the first layer receive the input
-            if( i == 0 ):
+            if i == 0:
+                # The first layer receive the input
                 self.net[i].a[1:] = self.data_X[self.idx, :].transpose()
-                continue
-                
-            self.net[i].z = self.net[i-1].W.dot(self.net[i-1].a)
-            self.net[i].set_activation()
-        
-
+            else:
+                self.net[i].z = self.net[i - 1].W.dot(self.net[i - 1].a)
+                self.net[i].set_activation()
 
     def train(self, X, Y, r, iterations, shuffle=False, check_grad=True):
-        #import pdb; pdb.set_trace()
         self.data_X = X
         self.data_Y = Y
 
-        # clean mse and ll past histories
+        # Start a new MSE and LL histories
         self.mse_history = []
         self.ll_history = []
-        
 
-        # order to roll over the samples
+        # Order to roll over the samples
         data_X_ids_order = np.arange(self.data_X.shape[0], dtype=int)
         if shuffle:
             np.random.shuffle(data_X_ids_order)
 
-        
-        # no. of iterations to get an epoch
-        itr_to_epoch = self.data_X.shape[0] / self.batch_size
+        # Compute how many iterations is needed to reach one epoch
+        itr_to_epoch = int(self.data_X.shape[0] / self.batch_size)
+        if itr_to_epoch == 0:
+            sys.exit("The batch size is greater than the available sample")
+
         j = 0
         for i in np.arange(iterations):
-            self.idx = data_X_ids_order[(j*self.batch_size):((j+1)*self.batch_size)]
-
-            # mark position into data chunks
+            self.idx = data_X_ids_order[(j * self.batch_size):((j + 1) * self.batch_size)]
+            # Mark data chunk position
             j = j + 1
-            # reset if reach the mark if reach the whole sample
-            if j >= itr_to_epoch: j = 0
-
-            self.feed_forward_NN()           
-            # register the history of optimization parameters
+            if j >= itr_to_epoch:
+                # Reset `j` if it completed one epoch
+                j = 0
+            self.feed_forward_NN()
             self.metric_register()
-
             self.update_weights(r, check_grad)
+            if i >= 4:
+                # Turn off gradient checking after 4 iterations
+                check_grad = False
 
-            # turn off gradient checking
-            if i >= 4: check_grad = False
-        
-        # compute the optimization values from the last adjusted weights
+        # Compute the optimization values from the last adjusted weights
         self.metric_register()
 
-
-    
     def predict(self, x_i):
-        # n_predictions control the size of the matrix which is to work
-        # based on the number of samples sent to predict function
         x_i = x_i.transpose()
-        
         if len(x_i.shape) == 1:
             x_i = np.expand_dims(x_i, 1)
-        n_predictions = x_i.shape[1]
 
-        # feed foward process
+        n_predictions = x_i.shape[1]
+        # Feed foward process
         for i in np.arange(0, self.layer_out_id + 1, dtype=int):
-            if( i == 0 ):
+            if i == 0:
                 self.net[i].a[1:, :n_predictions] = x_i
-                continue
-            
-            self.net[i].z[:, :n_predictions] = self.net[i-1].W.dot(self.net[i-1].a[:, :n_predictions])
-            self.net[i].set_activation()
-        
+            else:
+                self.net[i].z[:, :n_predictions] = self.net[i - 1].W.dot(self.net[i - 1].a[:, :n_predictions])
+                self.net[i].set_activation()
         predictions = self.net[self.layer_out_id].a[:, :n_predictions]
         return predictions
-    
-    
-    
+
     def train_fitted(self):
         train_fitted = np.array(([]))
         for i in np.arange(self.data_X.shape[0], dtype=int):
             p = self.predict(self.data_X[i])
             train_fitted = np.append(train_fitted, p)
-        
         return train_fitted
-    
 
-    
     def mean_squared_error(self):
-        #h = self.net[self.layer_out_id].a
         h = self.predict(self.data_X[self.idx])
         y = self.data_Y[self.idx]
-        mse = 0.5*np.power(y - h, 2)
-        # normalize by batch_size
+        mse = 0.5 * np.power(y - h, 2)
+        # Normalize by the batch_size
         mse = np.sum(mse) / self.batch_size
         return mse
 
-        
-        
-    
     def log_likelihood(self):
         h = self.predict(self.data_X[self.idx])
         y = self.data_Y[self.idx]
-        ll = y*np.log(h) + (1-y)*np.log(1-h)
+        ll = y * np.log(h) + (1 - y) * np.log(1 - h)
         # normalize by batch_size
         ll = np.sum(ll) / self.batch_size
-
-        # invert tz he signal to create the negative log-likelihood function
-        ll = -1*ll
         return ll
 
-
-    
     def metric_register(self):
         obj_value = self.objective_fun()
         self.obj_history.append(obj_value)
-        
